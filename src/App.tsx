4aa70201349afc3c4d7f842d12e1da8ac29166fc
Onur Buldukoglu <obuldukoglu@gmail.com>
2022-04-25 16:13:02 +0300

    add redux-saga

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 function App() {
+  fetch(
+    'https://reststop.randomhouse.com/resources/titles?keyword=Grisham%20Christmas'
+  )
+    .then((res) => res.json())
+    .then((json) => console.log(json));
   return <div className="App"></div>;
 }
 
