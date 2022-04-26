df34aa874ef4abf2e298dded7d67f0d0938011d5
Onur Buldukoglu <obuldukoglu@gmail.com>
2022-04-26 17:58:47 +0300

    add payment component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Header } from './features/header/Header';
+import { Payment } from './features/payment/Payment';
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
       <Header />
+      <Payment />
     </div>
   );
 }
