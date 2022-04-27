ed21815eead7b03fc185cc6f437bd98053617e8c
Onur Buldukoglu <obuldukoglu@gmail.com>
2022-04-27 14:05:35 +0300

    Change components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Header } from './features/header/Header';
 import { Payment } from './features/payment/Payment';
+import { Account } from './features/account/Account';
 
 function App(): JSX.Element {
   return (
     <div className="App">
       <Header />
-      <Payment />
+      <Account />
     </div>
   );
 }
